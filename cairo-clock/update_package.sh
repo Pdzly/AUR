694561a0b23a858d25d6bae9989a95bc4ed58050
rooki <rooki@rooki.xyz>
2025-11-23 23:31:32 +0100

    - Initialize cairo-clock package with PKGBUILD and patch - Add update script for AUR publishing - Remove compositor check from cairo-clock source - Set pkgver=0.3.4 pkgrel=5 for initial release

---
+++ cairo-clock/update_package.sh
@@ -0,0 +1,22 @@
+set -e
+
+git clone ssh://aur@aur.archlinux.org/cairo-clock.git || exit 1
+
+cp PKGBUILD cairo-clock
+cp cairo-clock.patch cairo-clock
+
+cd cairo-clock || exit 1
+makepkg --printsrcinfo > .SRCINFO
+
+current_version=$(grep "^pkgver=" PKGBUILD | cut -d'=' -f2)
+current_pkgrel=$(grep "^pkgrel=" PKGBUILD | cut -d'=' -f2)
+
+
+git add .
+git commit -m "Updating package to ${current_version} - ${current_pkgrel}"
+git push
+
+cd ..
+
+rm -rf cairo-clock
+echo "Successfully published package to AUR with the version ${current_version} - ${current_pkgrel}"
